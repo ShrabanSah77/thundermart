e3f25c65c808d6610a1b18d57a7b3b2db5bc2ac6
ShrabanSah77 <shrabanshah77@gmail.com>
2026-03-31 22:37:10 -0400

    end for today

--- nepmart/urls.py
+++ nepmart/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views  # relative import from the same app
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.index, name='index'),
     path('login_view/', views.login_view, name= 'login_view'),
     path('register_view/', views.register_view, name='register_view'),
 ]
--- nepmart/views.py
+++ nepmart/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def home(request):
-    return HttpResponse("This is my first view page!!")
+def index(request):
+    context = {'user_name': 'Shraban'}
+    return render(request, 'index.html', context)
 
 def login_view(request):
     return HttpResponse("This is my second view page!!")
