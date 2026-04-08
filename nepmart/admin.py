f9804033a24554acd0dc3890ee1972b419f4d208
ShrabanSah77 <shrabanshah77@gmail.com>
2026-04-08 01:16:21 -0400

    update in admin

--- nepmart/admin.py
+++ nepmart/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from .models import Customer
-from .models import Product
+from .models import Products
 
 # Register your models here.
 
 admin.site.register(Customer)
-admin.site.register(Product)
-
+admin.site.register(Products)
