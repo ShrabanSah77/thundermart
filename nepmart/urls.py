b208cff768fabf24e56c2172cb9624535b16c0ad
ShrabanSah77 <shrabanshah77@gmail.com>
2026-04-08 01:16:38 -0400

    urls

--- nepmart/urls.py
+++ nepmart/urls.py
@@ -3,6 +3,7 @@ from . import views  # relative import from the same app
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login_view/', views.login_view, name= 'login_view'),
-    path('register_view/', views.register_view, name='register_view'),
+    path('signIn_view/', views.signIn_view, name= 'signIn_view'),
+    path('Customer/', views.Customer, name = 'Customer'),
+    path('Products/', views.Products, name='Products'),
 ]
