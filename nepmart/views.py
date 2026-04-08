9549ff8f02b17fe5773700093378a78057756673
ShrabanSah77 <shrabanshah77@gmail.com>
2026-04-08 01:16:50 -0400

    views

--- nepmart/views.py
+++ nepmart/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Customer
+from .models import Products
 
 # Create your views here.
 def index(request):
     context = {'user_name': 'Shraban'}
     return render(request, 'index.html', context)
 
-def login_view(request):
+def signIn_view(request):
     return HttpResponse("This is my second view page!!")
 
-def register_view(request):
-    return HttpResponse("This is my third view page!!")
+def customer(request):
+    Customer = Customer.objects.all()
+    return HttpResponse(Customer)
 
-
+def product_list(request):
+    Products = Products.objects.all()
+    return HttpResponse(Products)
