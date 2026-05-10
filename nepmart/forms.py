3a41b8b66008ddea88c68587c04d6ef5c5b32aa1
ShrabanSah77 <shrabanshah77@gmail.com>
2026-05-09 23:41:54 -0400

    changes in views

---
+++ nepmart/forms.py
@@ -0,0 +1,4 @@
+from django import forms
+class LoginForm(forms.form):
+    username = forms.CharField(max_length=65)
+    password = forms.CharField(max_length= 65, widget=forms.PasswordInput)
--- nepmart/views.py
+++ nepmart/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-from .models import Customer, Product
 
 # Create your views here.
 def index(request):
@@ -17,7 +16,7 @@ def login_view(request):
 
         user = authenticate(request, username=username, password=password)
 
-        if user is not None:
+        if user:
             login(request, user)
             return redirect('home') #Redirect to the homepage
         else:
@@ -28,12 +27,32 @@ def login_view(request):
 def register_view(request):
     if request.method == ('POST'):
         username = request.POST.get('username: ')
+        email = request.POST.get('email')
         password = request.POST.get('password: ')
+
+        if not username or not email or not password:
+            messages.error(request, 'All fields are required')
+            return redirect('register')
+        
+        if User.objects.filter(username=username).exists():
+            messages.error(request, 'username already exists')
+
+        else:
+            User.objects.create_user(username = username, email=email, password=password)
+            messages.success(request, 'Account created successfully')
+            return redirect('login')
         
     return render(request, 'signIn/register.html')
 
-def forgotPassword_view(request):
-    return render(request, 'forgotpassword.html')
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+def forgot_password_view(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        messages.success(request, 'Password reset link sent(check console)')
+    return render(request, 'signIn/forgot_password.html')
 
 def customer_list(request):
     customer = Customer.objects.all()
