d7f0e35a9c49d2658215252e0463d6ec468ccbeb
ShrabanSah77 <shrabanshah77@gmail.com>
2026-05-10 11:10:07 -0400

    creating views

--- nepmart/views.py
+++ nepmart/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .models import Product, Customer
 
 # Create your views here.
 def index(request):
