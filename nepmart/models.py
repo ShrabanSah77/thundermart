c4dfc737a45f7e5675c3cc7ea6aea8967b8df069
ShrabanSah77 <shrabanshah77@gmail.com>
2026-05-10 11:09:59 -0400

    createing models

--- nepmart/models.py
+++ nepmart/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Customer(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -13,6 +15,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.IntegerField()
     description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
