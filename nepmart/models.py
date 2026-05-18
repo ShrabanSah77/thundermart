98da47bc3d743433f9b257861fc9e22e5c76c114
ShrabanSah77 <shrabanshah77@gmail.com>
2026-05-17 22:11:08 -0400

    creating checkout models

--- nepmart/models.py
+++ nepmart/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -23,3 +24,35 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+    
+# Models for order
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    address = models.TextField()
+    phone = models.CharField(max_length=20)
+
+    PAYMENT_CHOICE = {
+        ('COD', 'Cash on Delivery'),
+        ('CARD', 'Card'),
+    }
+
+    payment_method = models.CharField(max_length=10, choices=PAYMENT_CHOICE)
+    total_price = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+# Models for Ordered Items
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    quantity = models.IntegerField()
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.product.name
